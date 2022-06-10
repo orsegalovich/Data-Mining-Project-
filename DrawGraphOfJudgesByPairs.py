53bd53c6899502de88f6876814a0749437d0b842
Amirdot11 <amirdotan11@gmail.com>
2022-06-10 17:50:25 +0300

    Create Count of total cases each pair of judges have, calc ration and graph based on it

--- DrawGraphOfJudgesByPairs.py
+++ DrawGraphOfJudgesByPairs.py
@@ -5,9 +5,10 @@ import numpy as np
 
 df = pd.read_csv('PairOfJudgesAndMutualCaseCount.csv', low_memory=False, na_filter=False)
 G = nx.from_pandas_edgelist(df, source='judge_a', target='judge_b',
-                            edge_attr='appearances')
+                            edge_attr='ratio')
 
-widths = np.array([w for *_, w in G.edges.data('appearances')])
+edges = [(u, v) for (u, v, d) in G.edges(data=True) if d["ratio"] > 0.075]  # SET RATIO HERE
+widths = np.array([w*7 for *_, w in G.edges.data('ratio')])
 
 pos = nx.spring_layout(G, seed=7)  # positions for all nodes - seed for reproducibi
 
@@ -15,13 +16,13 @@ pos = nx.spring_layout(G, seed=7)  # positions for all nodes - seed for reproduc
 nx.draw_networkx_nodes(G, pos, node_size=600)
 
 # edges
-nx.draw_networkx_edges(G, pos, width=widths/1000)  # using a 10x scale factor here
+nx.draw_networkx_edges(G, pos, edgelist= edges, width=widths)  # using a 10x scale factor here
 
 # labels
-nx.draw_networkx_labels(G, pos, font_size=7, font_family="sans-serif")
+nx.draw_networkx_labels(G, pos, font_size=8, font_family="sans-serif")
 
 ax = plt.gca()
 ax.margins(0.03)
 plt.axis("off")
 plt.tight_layout()
-plt.savefig('.\Judges_connection_by_cases_calculated_by_pairs')
+plt.savefig('.\graph_pictures\pairs_by_ratio_0075_threshold')
---
+++ count_amount_of_cases_per_judge.py
@@ -0,0 +1,43 @@
+import pandas as pd
+
+df = pd.read_csv('LawDbWithSeniorityAndVerdictCountAndDuration.csv', low_memory=False, na_filter=False)
+df2 = pd.read_csv('PairOfJudgesAndMutualCaseCount.csv', low_memory=False, na_filter=False)
+
+
+# def get_judges_dict_with_vals_as_zero():
+#     judges_list = df['Name'].to_list()
+#     judges_dict = dict()
+#     for judge in judges_list:
+#         if judge not in judges_dict:
+#             judges_dict[judge] = 0
+#
+#
+#     return judges_dict
+#
+# judges_dict = get_judges_dict_with_vals_as_zero()
+
+judges_dict = dict()
+judges_name_by_cases = df['Name'].to_list()
+
+for name in judges_name_by_cases:
+    if name not in judges_dict:
+        judges_dict[name] = 1
+    else:
+        judges_dict[name] += 1
+
+
+
+for row_ind in df2.index:
+    to_add = judges_dict[df2["judge_a"][row_ind]] + judges_dict[df2["judge_b"][row_ind]]
+    df2.at[row_ind, 'sum_of_both_individual_appearances'] = to_add
+    mutual = df2.loc[row_ind, 'mutual_appearances']
+    df2.at[row_ind, 'ratio'] = mutual/to_add
+
+
+
+
+# df2 = pd.read_csv('PairOfJudgesAndMutualCaseCount.csv', encoding="utf-8-sig", index=False)
+
+df2.to_csv('PairOfJudgesAndMutualCaseCount.csv', encoding="utf-8-sig", index=False)
+
+print(df2.head())
