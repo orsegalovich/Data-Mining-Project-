c4033c5eaa4397bfeae2c4ce8546933418d869bb
orsegalovich <101465773+orsegalovich@users.noreply.github.com>
2022-06-10 18:09:04 +0300

    AVG seniority for overrule cases

--- Project.py
+++ Project.py
@@ -10,21 +10,24 @@ df['avg_seniority'] = pd.to_numeric(df['avg_seniority'],errors='coerce')
 def get_seniority_affect_of_overruling():
     count_overruled = 0
     sum_avg_seniority_overruled = 0
-    temp_case = "1000002.01"
+    temp_case = 1000002
     for i in range(len(df['ISCD_ID'])):
         if df['legalProcedure'][i] != "High Court of Justice" and df['outcomecourt'][i] == "Accepted":
-            if float(df['ISCD_ID'][i]) - float(temp_case) > 0.1:
-                temp_case = df['ISCD_ID'][i]
+            case = df['ISCD_ID'][i]
+            case = int(case)
+            if case != temp_case:
+                temp_case = case
                 count_overruled += 1
-                sum_avg_seniority_overruled += df["avg_seniority"][i]
-    return [sum_avg_seniority_overruled/count_overruled]
+                sum_avg_seniority_overruled += float(df["avg_seniority"][i])
+    return sum_avg_seniority_overruled/count_overruled
+
+
+print("Average seniority of judges in appeal cases overruled: " , get_seniority_affect_of_overruling())
+print("Average seniority of judges in all cases: " , df["avg_seniority"].mean())
+print("Average seniority of judges in Appeal cases: " , df.where(df['legalProcedure'] != "High Court of Justice")["avg_seniority"].mean())
 
 
 
-print(get_seniority_affect_of_overruling())
 
-# df_grouped = df.groupby(["ISCD_ID"])["avg_seniority"]
-#
-# print(df_grouped.head(10))
-# print(type(df_grouped))
+
 
